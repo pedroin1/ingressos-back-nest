ea96d4bb52c53ca3079769abe9e79cdca1b90c94
triad-pedro-bastos <pedro.bastos@triadpesquisa.com.br>
2024-06-19 18:02:31 -0300

    Configurnado o classvalidator

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { useContainer } from 'class-validator';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,9 @@ async function bootstrap() {
       transform: true,
     }),
   ); // configurando pipe de valiadacao
+
+  //permitindo o validator a utilizar os modulos
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   await app.listen(3000);
 }
